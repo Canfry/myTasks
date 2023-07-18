f826a7302210a911f3fc4153ba39c9de42586b64
fry <fry.land@hotmail.fr>
2023-07-18 12:17:56 +0200

    adding title

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,15 +1,21 @@
+import { Head } from "https://deno.land/x/fresh@1.2.0/runtime.ts";
 import Todos from "../islands/Todos.tsx";
 
 export default function Home() {
   return (
-    <body className="bg-gray-200">
-      <div className="my-10 px-5 rounded bg-white mx-auto flex max-w-screen-md flex-col justify-center py-12 h-[100%]">
-        <div className="mx-auto">
-          <h2 className="text-2xl font-bold mb-5 text-center">MY TASKS</h2>
-          {/* <img className="mx-auto" src="/logo.svg" alt="Fresh Logo" /> */}
+    <>
+      <Head>
+        <title>My Tasks with Fresh</title>
+      </Head>
+      <body className="bg-gray-200">
+        <div className="my-10 px-5 rounded bg-white mx-auto flex max-w-screen-md flex-col justify-center py-12 h-[100%]">
+          <div className="mx-auto">
+            <h2 className="text-2xl font-bold mb-5 text-center">MY TASKS</h2>
+            {/* <img className="mx-auto" src="/logo.svg" alt="Fresh Logo" /> */}
+          </div>
+          <Todos />
         </div>
-        <Todos />
-      </div>
-    </body>
+      </body>
+    </>
   );
 }
